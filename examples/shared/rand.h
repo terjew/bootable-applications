54f5d1f9c5b00ccda1e1b365e291c6aa2aeff163
Terje Wiesener <wiesener@gmail.com>
2025-05-02 00:31:30 +0200

    Switched to a supposedly better rand implementation

--- examples/shared/rand.h
+++ examples/shared/rand.h
@@ -1,21 +1,37 @@
-// https://gist.github.com/tommyettinger/46a874533244883189143505d203312c
+/*  
+Splitmix64
 
-unsigned int currentRand;
+Written in 2015 by Sebastiano Vigna (vigna@acm.org)
 
-void srand(unsigned int seed)
+To the extent possible under law, the author has dedicated all copyright
+and related and neighboring rights to this software to the public domain
+worldwide. This software is distributed without any warranty.
+
+See <http://creativecommons.org/publicdomain/zero/1.0/>. */
+
+/* This is a fixed-increment version of Java 8's SplittableRandom generator
+   See http://dx.doi.org/10.1145/2714064.2660195 and
+   http://docs.oracle.com/javase/8/docs/api/java/util/SplittableRandom.html
+
+   It is a very fast generator passing BigCrush, and it can be useful if
+   for some reason you absolutely want 64 bits of state. */
+
+static unsigned long rand_state; /* The state can be seeded with any value. */
+
+unsigned long rand_long()
 {
-  currentRand = seed;
+  unsigned long z = (rand_state += 0x9e3779b97f4a7c15);
+  z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
+  z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
+  return z ^ (z >> 31);
 }
 
-unsigned int rand(void)
+unsigned int rand()
 {
-  unsigned int z = (currentRand += 0x6D2B79F5UL);
-  z = (z ^ (z >> 15)) * (z | 1UL);
-  z ^= z + (z ^ (z >> 7)) * (z | 61UL);
-  return z ^ (z >> 14);
+  return (unsigned int)(rand_long() & 0xffffffff);
 }
 
-int randRange(int min, int max) {
-  return min + ((int)rand()) % (max - min + 1);
+void srand(unsigned long seed)
+{
+  rand_state = seed;
 }
-
